a7bee13cba2396478acee23588b322f05a9e7ee2
avraal <gammaclone@gmail.com>
2018-11-28 19:07:41 +0200

    added file with all url and keys

---
+++ src/keys.py
@@ -0,0 +1,7 @@
+__TOKEN__ = 'NTE0MTAwNDE5Nzk1MDkxNDc3.DtRr-w.bbhC5it6LykZsU8upnzBTwnfcfY'
+
+__get_all_tasks_url__ = 'http://taskbox.zzz.com.ua/pr/get_all_tasks.php'
+__reg_url__ = 'http://taskbox.zzz.com.ua/pr/reg.php'
+__push_task_url__ = 'http://taskbox.zzz.com.ua/pr/push_task.php'
+__validate_url__ = 'http://taskbox.zzz.com.ua/pr/validate.php'
+__get_user_url__ = 'http://taskbox.zzz.com.ua/pr/get_user.php'
